3b3c01c733fcd2b86a6a75bae9006a8f62218dce
Alonso <alonso12_284@outlook.com>
2021-12-23 12:03:47 -0600

    Setting Up

---
+++ Educational Codeforces Round 119 (Rated for Div. 2)/D.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+cd Documents/Github/CodeForces/'Educational Codeforces Round 119 (Rated for Div. 2)'
+g++ D.cpp -std=c++17
+*/
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	return 0;
+}
